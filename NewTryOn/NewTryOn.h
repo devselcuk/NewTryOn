71ba288f88b48b91d0b16aa14173d6e81ed79050
Selçuk YILDIZ <devselcukdev@gmail.com>
2020-04-24 13:42:10 +0300

    Initial Commit

---
+++ NewTryOn/NewTryOn.h
@@ -0,0 +1,19 @@
+//
+//  NewTryOn.h
+//  NewTryOn
+//
+//  Created by Enes on 24.04.2020.
+//  Copyright © 2020 KY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NewTryOn.
+FOUNDATION_EXPORT double NewTryOnVersionNumber;
+
+//! Project version string for NewTryOn.
+FOUNDATION_EXPORT const unsigned char NewTryOnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewTryOn/PublicHeader.h>
+
+
